13589ede0fcde7b179f79b785097cbc9237b7a9b
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-11 16:45:48 +0200

    Changes version to alpha

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'shellshocker'
 maintainer       'alt3.io'
 license          'MIT'
 description      'Uses Shellshocker to detect and mitigate Shellshock Bash Vulnerabilities.'
-version          '0.1'
+version          '0.0.1'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 supports 'ubuntu'
